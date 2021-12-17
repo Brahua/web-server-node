05cab34d81acdaa30f816fcdfbf3430cf861ec85
Josue Bravo <josuebh62@gmail.com>
2021-12-17 01:29:40 -0500

    disable x-powered-by cause is vulnerability detected by polaris

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const hbs = require("hbs");
 const app = express();
+app.disable('x-powered-by');
 const PORT = process.env.PORT;
 
 //Handlebars
